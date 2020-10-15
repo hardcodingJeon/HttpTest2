4a393b9bb3f77fd1d53378eb5a150dee0f114465
hardcodingJeon <sonlcr1@naver.com>
2020-10-15 16:23:23 +0900

    master init commit

--- app/src/main/java/com/example/httptest2/MainActivity.java
+++ app/src/main/java/com/example/httptest2/MainActivity.java
@@ -2,22 +2,7 @@ package com.example.httptest2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
